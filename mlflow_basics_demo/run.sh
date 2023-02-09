8abb29e9a092a927352fb16d9dada304542632a1
haruiz <henryruiz22@gmail.com>
2023-02-08 23:30:18 -0600

    mlflow-basics demo done

--- mlflow_basics_demo/run.sh
+++ mlflow_basics_demo/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 IMAGE_NAME=mlflow_demo_image
-CONTAINER_NAME=mlflow_jupyter
-docker build -t mlflow_demo .
+CONTAINER_NAME=mlflow_demo
+docker build -t $IMAGE_NAME .
 
 if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
     docker kill $CONTAINER_NAME
 fi
 docker rm $CONTAINER_NAME
 # shellcheck disable=SC2046
-docker run --name $CONTAINER_NAME -p 8888:8888 -p 4000:4000 -v $(pwd):/home/jovyan/ -it $IMAGE_NAME
+# rm = automatically remove the container when it exits
+docker run --name $CONTAINER_NAME  --user root -e GRANT_SUDO=yes --rm -p 8888:8888 -p 4000:4000 -v $(pwd):/home/jovyan/ -it $IMAGE_NAME
